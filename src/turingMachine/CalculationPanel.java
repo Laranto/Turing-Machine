5fcb54c486054aa20e42681883f51ef4f635a3a5
sutermi <suter.mi@gmail.com>
2013-11-11 20:44:21 +0100

    added CalculationPanel

---
+++ src/turingMachine/CalculationPanel.java
@@ -0,0 +1,27 @@
+package turingMachine;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JPanel;
+
+import turingMachine.logic.Tape;
+import turingMachine.logic.Tape.StepResult;
+
+/**
+ * Shows all the calculation steps
+ * @author Michael
+ *
+ */
+public class CalculationPanel extends JPanel implements Observer{
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if(arg instanceof Tape){
+			System.out.println(((Tape)arg).getCurrentState().getIdentifier());
+		}else if(arg instanceof StepResult){
+			System.out.println(((StepResult)arg));
+		}
+	}
+
+}
